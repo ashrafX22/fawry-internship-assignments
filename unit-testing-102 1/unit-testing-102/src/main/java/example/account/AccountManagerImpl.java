474b7f8617f18be32d40c54a7e0a7059f362d933
ashrafX22 <ashrafmaged593@gmail.com>
2024-12-11 12:12:53 +0200

    [fix] MAX_CREDIT and change the condition

--- unit-testing-102 1/unit-testing-102/src/main/java/example/account/AccountManagerImpl.java
+++ unit-testing-102 1/unit-testing-102/src/main/java/example/account/AccountManagerImpl.java
@@ -1,7 +1,7 @@
 package example.account;
 
 public class AccountManagerImpl implements AccountManager {
-    private static final int MAX_CREDIT = 1000;
+    private static final int MAX_CREDIT = -1000;
     @Override
     public void deposit(Customer customer, int amount) {
         customer.setBalance(customer.getBalance() + amount);
@@ -14,7 +14,7 @@ public class AccountManagerImpl implements AccountManager {
         if (expectedBalance < 0) {
             if (!customer.isCreditAllowed()) {
                 return "insufficient account balance";
-            } else if (expectedBalance > MAX_CREDIT && !customer.isVip()) {
+            } else if (expectedBalance < MAX_CREDIT && !customer.isVip()) {
                 return "maximum credit exceeded";
             }
         }
