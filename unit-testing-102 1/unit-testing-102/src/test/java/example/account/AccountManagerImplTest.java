2a51a9e0a4753c66f4362a5371e0e726741d4691
ashrafX22 <ashrafmaged593@gmail.com>
2024-12-11 12:47:19 +0200

    [wip] tesitng deposite

--- unit-testing-102 1/unit-testing-102/src/test/java/example/account/AccountManagerImplTest.java
+++ unit-testing-102 1/unit-testing-102/src/test/java/example/account/AccountManagerImplTest.java
@@ -8,7 +8,10 @@ class AccountManagerImplTest {
 
     @Test
     void deposit () {
-
+        AccountManager accountManager = new AccountManagerImpl();
+        Customer customer = new Customer("ahsraf",1000,false,false);
+        accountManager.deposit(customer,500);
+        assertEquals(1500, customer.getBalance());
     }
 
     @Test
@@ -23,25 +26,23 @@ class AccountManagerImplTest {
     void withdraw_with_insufficient_balance_by_normal_customer() {
         AccountManager accountManager = new AccountManagerImpl();
         Customer customer = new Customer("ahsraf",1000,false,false);
-        String resualt = accountManager.withdraw(customer,2000);
-        assertEquals("insufficient account balance", resualt);
+        String result = accountManager.withdraw(customer,2000);
+        assertEquals("insufficient account balance", result);
     }
 
     @Test
     void withdraw_with_maximum_credit_by_normal_customer() {
         AccountManager accountManager = new AccountManagerImpl();
         Customer customer = new Customer("ahsraf",1000,true,false);
-        String resualt = accountManager.withdraw(customer,2000);
-        assertEquals("success", resualt);
+        String result = accountManager.withdraw(customer,2000);
+        assertEquals("success", result);
     }
 
     @Test
     void withdraw_with_maximum_credit_exceeded_by_normal_customer() {
         AccountManager accountManager = new AccountManagerImpl();
         Customer customer = new Customer("ahsraf",900,true,false);
-        String resualt = accountManager.withdraw(customer,2000);
-        assertEquals("maximum credit exceeded", resualt);
+        String result = accountManager.withdraw(customer,2000);
+        assertEquals("maximum credit exceeded", result);
     }
-
-
 }
