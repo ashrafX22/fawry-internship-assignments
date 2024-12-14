4562339d948c3705437c7fc956598c4a42700e40
ashrafX22 <ashrafmaged593@gmail.com>
2024-12-14 13:11:09 +0200

    [wip] add testcases for successful payment and product out of stock payment

---
+++ unit-testing-102 1/unit-testing-102/src/test/java/example/store/StoreImplTest.java
@@ -0,0 +1,52 @@
+package example.store;
+
+import example.account.AccountManager;
+import example.account.AccountManagerImpl;
+import example.account.Customer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreImplTest {
+
+    @Test
+    void givenProductWithValidQuantity_whenWithdrawingWithSuccess_thenReturnProductQuantityLessByOne() {
+        //Given
+        Product product = new Product();
+        Customer customer = new Customer();
+        product.setQuantity(7);
+        product.setPrice(700);
+
+        //When
+        AccountManager accountManager = Mockito.mock(AccountManagerImpl.class);
+        Mockito.when(accountManager.withdraw(customer, product.getPrice())).thenReturn("success");
+        Store store = new StoreImpl(accountManager);
+        store.buy(product,customer);
+
+        //Then
+        Assertions.assertEquals(6,product.getQuantity());
+    }
+
+    @Test
+    void givenProductWithZeroQuantity_whenWithdrawing_thenThrowException() {
+        //Given
+        Product product = new Product();
+        Customer customer = new Customer();
+        product.setQuantity(0);
+        product.setPrice(700);
+
+        //When
+        AccountManager accountManager = Mockito.mock(AccountManagerImpl.class);
+        Mockito.when(accountManager.withdraw(customer, product.getPrice())).thenReturn("success");
+        Store store = new StoreImpl(accountManager);
+
+        //Then
+        assertThrows(RuntimeException.class, () -> store.buy(product, customer));
+    }
+
+
+}
