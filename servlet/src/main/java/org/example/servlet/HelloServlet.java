1481042156bc1d95d9a3a9bf89beb94c3330fa07
ashrafX22 <ashrafmaged593@gmail.com>
2024-12-18 13:25:07 +0200

    [add] assignment materials

---
+++ servlet/src/main/java/org/example/servlet/HelloServlet.java
@@ -0,0 +1,16 @@
+package org.example.servlet;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello World!</h1>");
+    }
+}
